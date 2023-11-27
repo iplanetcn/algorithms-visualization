dc1ca6f9cb1e26e197183f557b2e1f6da4a83ea1
John <iplanetcn@gmail.com>
2023-11-27 17:33:21 +0800

    optimize code

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -3,7 +3,10 @@ import java.awt.Toolkit
 
 private const val WINDOW_WIDTH = 800
 private const val WINDOW_HEIGHT = 600
+private const val N = 200
+
 val SCREEN_SIZE: Dimension = Toolkit.getDefaultToolkit().screenSize
+
 fun main() {
-    AlgorithmsVisualizer(WINDOW_WIDTH, WINDOW_HEIGHT, 200)
+    AlgorithmsVisualizer(WINDOW_WIDTH, WINDOW_HEIGHT, N)
 }
